2128c595cee13f6e8a3dfd4fc368cf9a463ca484
MarilynDCosta <mdc63@bath.ac.uk>
2025-09-08 14:57:48 +0100

    W3Schools Python Section 6

---
+++ W3Schools/Backend/Python/6_DataTypes.py
@@ -0,0 +1,16 @@
+# W3Schools - Python
+# Section 6: Data Types
+
+# Built-In Data Types:
+# Text Type         -> str
+# Numeric Types     -> int, float, complex
+# Sequence Types    -> list, tuple, range
+# Mapping Type      -> dict
+# Set Types         -> set, frozenset
+# Boolean Type      -> bool
+# Binary Types      -> bytes, bytearray, memoryview
+# None Type         -> NoneType
+
+# You can get the data type of an object by using the type() function:
+x = 5
+print(type(x)) # integer
