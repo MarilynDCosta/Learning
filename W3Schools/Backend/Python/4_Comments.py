291d49c39f3be64cb01c398a85c513ee09ede946
MarilynDCosta <mdc63@bath.ac.uk>
2025-09-08 13:30:38 +0100

    W3Schools Python Section 4

---
+++ W3Schools/Backend/Python/4_Comments.py
@@ -0,0 +1,27 @@
+# W3Schools - Python
+# Section 4: Comments
+
+
+# Use a hash sign to make a comment in Python
+# This is a comment
+
+# Use comments to allow Python to ignore the rest of the line
+print("Hello World") # This is a comment
+
+#You can use comments to prevent Python executing code
+# print("Marilyn is coding")
+print("Hello Marilyn")
+
+# Use a hash sign on multiple lines ti create multiline comments
+# This is a comment
+# written in
+# more than one line
+print("Hello Python")
+
+# You can do a multiline comment as a multiline string too
+"""
+This is a comment
+written in
+more than one line
+"""
+print("Hello Friends")
