ff3bf2980443152efff6c03f9d5915c06f4888ea
MarilynDCosta <mdc63@bath.ac.uk>
2025-10-03 10:20:09 +0100

    W3Schools Python Section 18

---
+++ W3Schools/Backend/Python/18_WhileLoops.py
@@ -0,0 +1,52 @@
+# W3Schools - Python
+# Section 18: While Loops
+
+
+# Python has two primitive loop commands: while loops and for loops.
+
+# The while loop can execute a set of statements as long as a condition is true.
+
+def countdown(startNumber):
+    while startNumber > 0:
+        print(startNumber)
+        startNumber -= 1
+
+countdown(10)
+
+# While loops require relevant variables to be ready.
+# For the example above, startNumber needed to be defined and it was set to 10 as a parameter within a function.
+
+# With a break statement, you can stop a while loop even if the condition is still true
+
+def countdownToFive(startNumber):
+    while startNumber >= 0:
+        print(startNumber)
+        if startNumber <= 5:
+            break
+        startNumber -= 1
+
+countdownToFive(10)
+
+# You can use a continue statement to stop a current iteration of a while loop and continue with the next iteration.
+
+def evenCountdown(startNumber):
+    while startNumber >= 0:
+        if startNumber % 2 == 0:
+            print(startNumber)
+        else:
+            startNumber -= 1
+            continue
+        startNumber -= 1
+
+evenCountdown(10)
+
+# You can use an else statement to run a block of code when a condition is no longer true.
+
+def bomb(startNumber):
+    while startNumber >= 1:
+        print(startNumber)
+        startNumber -= 1
+    else:
+        print("Bang")
+
+bomb(10)
